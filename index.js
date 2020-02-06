2c50ff0d29406c1bdf48d17ae0f4bc3b47f64bc3
ramon96 <rfm1@live.nl>
2020-02-06 23:51:23 +0100

    added the hammer functionality, and the homey

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if(msg.content === `${process.env.prefix}yeet`){
-        msg.reply("oh homey");
+    // TODO: write a switch case for this shit
+    if(msg.content.startsWith(`${process.env.prefix}Game and watch`)){
+         let rng = Math.floor(Math.random() * 9 ) + 1;
+        if (rng == 9){
+            let victim = msg.member;
+            // je bent de lul
+            msg.reply("The hammer rolled a " + rng + ". Get fucked!");
+
+            victim.kick("The hammer rolled a 9.")
+            .then(()=> console.log("succes"))
+            .catch(console.error);
+            msg.channel.send("Beep!", {file:"https://www.ssbwiki.com/images/thumb/a/a3/GameWatchSide2-SSB4.png/200px-GameWatchSide2-SSB4.png"})
+        }
+        else{
+            msg.reply("You have been hit by a " + rng);
+            msg.channel.send("Beep!", {file:"https://www.ssbwiki.com/images/thumb/e/ec/GameWatchSide1-SSB4.png/200px-GameWatchSide1-SSB4.png"})
+        }
+
+    }
+    else if(msg.content.startsWith(`${process.env.prefix}Homey`)){
+        msg.channel.send("Ohh Homeeyyy ", {tts:true});
     }
 });
 
