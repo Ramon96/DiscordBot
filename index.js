ff468dc5988d49278c5e80d1cf661b28c9c1867c
Ramon96 <rfm1@live.nl>
2020-02-07 12:46:52 +0100

    changed commands to lowercase

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('message', msg => {
     // TODO: write a switch case for this shit
     let message = msg.content.toLowerCase();
-    if(message.startsWith(`${process.env.prefix}Game and watch`)){
+    if(message.startsWith(`${process.env.prefix}game and watch`)){
          let rng = Math.floor(Math.random() * 9 ) + 1;
         if (rng == 9){
             let victim = msg.member;
@@ -27,10 +27,10 @@ client.on('message', msg => {
         }
 
     }
-    else if(message.startsWith(`${process.env.prefix}Homey`)){
+    else if(message.startsWith(`${process.env.prefix}homey`)){
         msg.channel.send("Ohh Homeeyyy ", {tts:true});
     }
-    else if(message.startsWith(`${process.env.prefix}Roll`)){
+    else if(message.startsWith(`${process.env.prefix}roll`)){
         let roll = Math.floor(Math.random() * 100 ) + 1;
         msg.reply(" has rolled " + roll);
     }
