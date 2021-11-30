d5e6532d86e970a6b4521924965eae15188ee202
ramon96 <rfm1@live.nl>
2021-11-30 14:11:28 +0100

    removed join call

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -4,13 +4,16 @@ module.exports = {
         if (oldMember.channelId === null) {
             // user joins a channel
             if (newMember.id == 275998536162738179) {
-                client.channels.cache.get('867074325824012382').send("Hallo Daan, Julian is in de discord server.");
+                // Julian
+                // client.channels.cache.get('867074325824012382').send("Hallo Daan, Julian is in de discord server.");
             }
             else if (newMember.id == 291296782187495424) {
-                client.channels.cache.get('867074325824012382').send("Kijk daar heb je DANIEL XDDDDDDDDD.");
+                // Daniel
+                // client.channels.cache.get('867074325824012382').send("Kijk daar heb je DANIEL XDDDDDDDDD.");
             }
             else if (newMember.id == 131124125996548096) {
-                client.channels.cache.get('867074325824012382').send("Good to see you again, my king <@131124125996548096> o7");
+                // Daan
+                // client.channels.cache.get('867074325824012382').send("Good to see you again, my king <@131124125996548096> o7");
             }
         } else if (newMember === null) {
             // user leaves
