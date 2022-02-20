353a7ea77260e23fa87210a0543e929adc1d16b4
ramon96 <rfm1@live.nl>
2022-02-20 03:08:33 +0100

    als dit het is ben ik confused

--- commands/normalcommands/dailymessage.js
+++ commands/normalcommands/dailymessage.js
@@ -11,7 +11,7 @@ module.exports = {
             const user = guild.members.cache.random().user;
             message.delete({ timeout: 100 }).catch(console.error);
 
-            while(user.id === '675080598355705899' || user.id == '641016805434851349' || user.id == '682137805249445898' || user.id == '878726499526647838' || user.id == '281524615975534592'){
+            while(user.id === '675080598355705899' || user.id === '641016805434851349' || user.id === '682137805249445898' || user.id === '878726499526647838' || user.id === '281524615975534592'){
                 user = guild.members.cache.random().user;
             }
             
