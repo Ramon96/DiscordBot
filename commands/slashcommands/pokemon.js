98040732a353b8f6e20fa834ac48f6b39b588e3b
Ramon96 <rfm1@live.nl>
2023-02-12 01:33:18 +0100

    shiny pokemon update with button

--- commands/slashcommands/pokemon.js
+++ commands/slashcommands/pokemon.js
@@ -1,8 +1,30 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 const fetchPokemon = require("../../helpers/api/pokemon/fetchPokemon");
 const pokeTypes = require("poke-types");
 const getKeyByValue = require("../../helpers/functions/keyByValue");
+const capatalize = require("../../helpers/functions/capatalize");
+
+const colours = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -29,7 +51,8 @@ module.exports = {
       }
 
       const sprite = data.sprites.front_default;
-      const name = data.name;
+      const shinySprite = data.sprites.front_shiny;
+      const name = capatalize(data.name);
       const type = data.types.map((t) => t.type.name).join(", ");
       const stats = data.stats
         .map((s) => `${s.stat.name}: ${s.base_stat}`)
@@ -63,26 +86,13 @@ module.exports = {
         .filter((a) => a.is_hidden == false)
         .map((a) => a.ability.name)
         .join(", ");
-      const colours = {
-        normal: "#A8A77A",
-        fire: "#EE8130",
-        water: "#6390F0",
-        electric: "#F7D02C",
-        grass: "#7AC74C",
-        ice: "#96D9D6",
-        fighting: "#C22E28",
-        poison: "#A33EA1",
-        ground: "#E2BF65",
-        flying: "#A98FF3",
-        psychic: "#F95587",
-        bug: "#A6B91A",
-        rock: "#B6A136",
-        ghost: "#735797",
-        dragon: "#6F35FC",
-        dark: "#705746",
-        steel: "#B7B7CE",
-        fairy: "#D685AD",
-      };
+
+      const row = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId(`shiny-${data.name}`)
+          .setLabel("Shiny form")
+          .setStyle(1)
+      );
 
       const embed = new MessageEmbed()
         .setTitle(name)
@@ -97,10 +107,23 @@ module.exports = {
         .addField("4x weakness", weakness4x, true)
         .addField("immunity", immunity, true);
 
+      const shinyEmbed = new MessageEmbed()
+        .setTitle(`Shiny ${name}`)
+        .setColor(colours[data.types[0].type.name])
+        .setImage(shinySprite);
+
+      const collector = interaction.channel.createMessageComponentCollector({});
+
       await interaction.deferReply();
-      await interaction.editReply(`Getting ${pokemon}'s stats.`);
       await interaction.followUp({
         embeds: [embed],
+        components: [row],
+      });
+
+      collector.once("collect", async (button) => {
+        await button.deferReply();
+
+        await button.editReply({ embeds: [shinyEmbed], components: [] });
       });
     } catch (error) {
       console.error(error);
