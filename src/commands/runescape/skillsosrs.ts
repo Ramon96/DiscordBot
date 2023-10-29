493f756901e6314e5067b6571017fb978ab6b958
Ramon96 <rfm1@live.nl>
2023-10-29 14:39:23 +0100

    match fields with the ui

--- src/commands/runescape/skillsosrs.ts
+++ src/commands/runescape/skillsosrs.ts
@@ -45,15 +45,149 @@ export default new Command({
     });
     embed.setThumbnail("https://i.imgur.com/oImaKtY.png");
 
-    for (const skill of Object.keys(stats.main.skills).filter(
-      (skill) => skill !== "overall"
-    )) {
-      embed.addFields({
-        name: osrsSkills[skill].emoji,
-        value: `${stats.main.skills[skill].level}`,
-        inline: true,
-      });
-    }
+    embed.addFields({
+      name: osrsSkills.attack.emoji,
+      value: `${stats.main.skills.attack.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.hitpoints.emoji,
+      value: `${stats.main.skills.hitpoints.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.mining.emoji,
+      value: `${stats.main.skills.mining.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.strength.emoji,
+      value: `${stats.main.skills.strength.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.agility.emoji,
+      value: `${stats.main.skills.agility.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.smithing.emoji,
+      value: `${stats.main.skills.smithing.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.defence.emoji,
+      value: `${stats.main.skills.defence.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.herblore.emoji,
+      value: `${stats.main.skills.herblore.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.fishing.emoji,
+      value: `${stats.main.skills.fishing.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.ranged.emoji,
+      value: `${stats.main.skills.ranged.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.thieving.emoji,
+      value: `${stats.main.skills.thieving.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.cooking.emoji,
+      value: `${stats.main.skills.cooking.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.prayer.emoji,
+      value: `${stats.main.skills.prayer.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.crafting.emoji,
+      value: `${stats.main.skills.crafting.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.firemaking.emoji,
+      value: `${stats.main.skills.firemaking.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.magic.emoji,
+      value: `${stats.main.skills.magic.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.fletching.emoji,
+      value: `${stats.main.skills.fletching.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.woodcutting.emoji,
+      value: `${stats.main.skills.woodcutting.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.runecraft.emoji,
+      value: `${stats.main.skills.runecraft.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.slayer.emoji,
+      value: `${stats.main.skills.slayer.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.farming.emoji,
+      value: `${stats.main.skills.farming.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.construction.emoji,
+      value: `${stats.main.skills.construction.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.hunter.emoji,
+      value: `${stats.main.skills.hunter.level}`,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: osrsSkills.overall.emoji,
+      value: `${stats.main.skills.overall.level}`,
+      inline: true,
+    });
 
     embed.addFields({
       name: "Clue Scrolls",
